2e34ba922d88657618b87a35871f28311d75f510
Leonardo Barbosa <leonardo.barbosaper@gmail.com>
2025-07-03 13:46:46 -0300

    Primeira classe: Position

---
+++ Trabalhos em Java/Projeto Sistema Jogo de Xadrez/projeto_jogo_xadrez/src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+import boardgame.Position;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Position ps = new Position(3, 2);
+		System.out.println(ps);
+	}
+
+}
