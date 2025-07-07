aafc317a5f6d7c2f9e3114a6b39272a5f030ee11
Leonardo Barbosa <leonardo.barbosaper@gmail.com>
2025-07-07 14:54:37 -0300

    Imprimindo movimentos possiveis

--- Trabalhos em Java/Projeto Sistema Jogo de Xadrez/projeto_jogo_xadrez/src/application/Program.java
+++ Trabalhos em Java/Projeto Sistema Jogo de Xadrez/projeto_jogo_xadrez/src/application/Program.java
@@ -25,6 +25,10 @@ public class Program {
 				System.out.print("Source: ");
 				ChessPosition source = UI.readChessPosition(sc);
 				
+				boolean[][] possibleMoves = ch.possibleMoves(source);
+				UI.clearScreen();
+				UI.printBoard(ch.getPieces(), possibleMoves);
+				
 				System.out.println();
 				System.out.print("Target: ");
 				ChessPosition target = UI.readChessPosition(sc);
