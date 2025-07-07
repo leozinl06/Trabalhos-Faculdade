0f5fe17d11829e6e64d385b316460d03c70a6bc7
Leonardo Barbosa <leonardo.barbosaper@gmail.com>
2025-07-07 15:44:23 -0300

    Lidando com pecas capturadas

--- Trabalhos em Java/Projeto Sistema Jogo de Xadrez/projeto_jogo_xadrez/src/application/Program.java
+++ Trabalhos em Java/Projeto Sistema Jogo de Xadrez/projeto_jogo_xadrez/src/application/Program.java
@@ -1,6 +1,8 @@
 package application;
 
+import java.util.ArrayList;
 import java.util.InputMismatchException;
+import java.util.List;
 import java.util.Scanner;
 
 import chess.ChessException;
@@ -13,13 +15,14 @@ public class Program {
 	public static void main(String[] args) {
 		
 		Scanner sc = new Scanner(System.in);
-		
 		ChessMatch ch = new ChessMatch();
+		List<ChessPiece> captured = new ArrayList<>();
+		
 		
 		while (true) {
 			try {
 				UI.clearScreen();
-				UI.printMatch(ch);
+				UI.printMatch(ch, captured);
 				
 				System.out.println();
 				System.out.print("Source: ");
@@ -35,6 +38,9 @@ public class Program {
 				
 				ChessPiece capturedPiece = ch.performChessMove(source, target);
 			
+				if (capturedPiece != null) {
+					captured.add(capturedPiece);
+				}
 			} 
 			catch (ChessException e) {
 				System.out.println(e.getMessage());
