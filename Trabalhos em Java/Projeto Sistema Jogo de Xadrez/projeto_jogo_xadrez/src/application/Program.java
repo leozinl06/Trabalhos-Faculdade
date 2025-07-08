707b969ddc95a435d4d9ea4c90252851777ec3f8
Leonardo Barbosa <leonardo.barbosaper@gmail.com>
2025-07-08 17:44:27 -0300

    Corrigindo - tratamento melhor para movimento Promotion

--- Trabalhos em Java/Projeto Sistema Jogo de Xadrez/projeto_jogo_xadrez/src/application/Program.java
+++ Trabalhos em Java/Projeto Sistema Jogo de Xadrez/projeto_jogo_xadrez/src/application/Program.java
@@ -44,7 +44,11 @@ public class Program {
 				
 				if (ch.getPromoted() != null) {
 					System.out.print("Enter piece for promotion (B/N/R/Q): ");
-					String type = sc.next();
+					String type = sc.next().toUpperCase();
+					while (!type.equals("B") && !type.equals("N") && !type.equals("R") && !type.equals("Q")) {
+						System.out.print("Invalid value! Enter piece for promotion (B/N/R/Q): ");
+						type = sc.next().toUpperCase();
+					}
 					ch.replacePromotedPiece(type);
 				}
 			} 
